d7a8c5c764b62884623b7e177af9130e8414a151
Ryotaro Hoshina <hoshina12345@gmail.com>
2021-03-30 16:08:00 +0900

    add capture sink test

---
+++ v4l2sinkcapture.cpp
@@ -0,0 +1,44 @@
+#include "v4l2sink.hpp"
+#include "opencv2/opencv.hpp"
+#include <stdio.h>
+
+int main(int argc, char**argv)
+{
+	if(argc < 3) 
+	{
+		printf("test needs: output_device input_device\n");
+		return -1;
+	}
+
+	v4l2sink sink;
+
+	if(!sink.open(argv[1]))
+	{
+		printf("Failed opening\n");
+		return -1;
+	}
+
+	int id = atoi(argv[2]);
+	cv::VideoCapture cap(id);
+	if(!cap.isOpened())
+	{
+		printf("not opened %d\n",id);
+		return -1;
+	}
+	else
+		printf("opened %d\n",id);
+
+	while(true)
+	{
+		cv::Mat img;
+		cap >> img;
+		printf("loaded %d %d %d\n",img.cols,img.rows,img.channels());
+		if(img.rows != 0)
+		{
+	    int r = sink.init(img.cols,img.rows,img.channels() == 1 ? v4l2sink::GRAY: v4l2sink::RGB);
+	    r = sink.write((const char*)img.data,img.cols*img.rows*img.channels());
+		}
+	}
+
+	return 0;
+}
